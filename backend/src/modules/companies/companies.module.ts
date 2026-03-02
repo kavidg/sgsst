f9e4469b4758a26bf351ad75fcc0785154e1dc9c
kavidg <kevin96gg@hotmail.com>
2026-03-02 09:50:13 -0500

    Config AuthModule in companies.module

--- backend/src/modules/companies/companies.module.ts
+++ backend/src/modules/companies/companies.module.ts
@@ -4,9 +4,11 @@ import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
 import { Company, CompanySchema } from './schemas/company.schema';
 import { User, UserSchema } from '../users/schemas/user.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([
       { name: Company.name, schema: CompanySchema },
       { name: User.name, schema: UserSchema },
