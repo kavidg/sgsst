ef90b317e286517880e044461303c097d8b9c634
kavidg <146221820+kavidg@users.noreply.github.com>
2026-03-05 15:45:01 -0500

    fix(backend): import auth module into evaluations module

--- backend/src/evaluations/evaluations.module.ts
+++ backend/src/evaluations/evaluations.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../modules/auth/auth.module';
 import { EvaluationsController } from './evaluations.controller';
 import { EvaluationsService } from './evaluations.service';
 import { Evaluation, EvaluationSchema } from './schemas/evaluation.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Evaluation.name, schema: EvaluationSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Evaluation.name, schema: EvaluationSchema }]),
+    AuthModule,
+  ],
   controllers: [EvaluationsController],
   providers: [EvaluationsService],
 })
