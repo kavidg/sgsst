151076796464facee435a666a2c355aa72d2e79b
kavidg <146221820+kavidg@users.noreply.github.com>
2026-05-25 16:58:36 -0500

    fix(copasst): import AuthModule for FirebaseAuthGuard DI

--- backend/src/modules/copasst/copasst.module.ts
+++ backend/src/modules/copasst/copasst.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AlertsModule } from '../alerts/alerts.module';
+import { AuthModule } from '../auth/auth.module';
 import { CopasstController } from './copasst.controller';
 import { CopasstService } from './copasst.service';
 import { CopasstPeriod, CopasstPeriodSchema } from './schemas/copasst.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: CopasstPeriod.name, schema: CopasstPeriodSchema }]), AlertsModule],
+  imports: [MongooseModule.forFeature([{ name: CopasstPeriod.name, schema: CopasstPeriodSchema }]), AlertsModule, AuthModule],
   controllers: [CopasstController],
   providers: [CopasstService],
 })
